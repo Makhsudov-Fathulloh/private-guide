bbe92a8bedabefe6877e31974ee26eed73288788
Makhsudov Fathulloh <mr.makhsudov@gmail.com>
2024-04-23 18:39:41 +0500

    add javascript Function

---
+++ javascript/pages/3.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=0);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/css/style.css">
+    <link rel="stylesheet" href="/css/css/main.css">
+
+    <title>Introduction to Javascript</title>
+</head>
+
+<body>
+
+<div>
+    <ul>
+        <li><b><a href="/index.php">Home</a></b></li>
+        <li><b><a href="/javascript/index.php">Javascript</a></b></li>
+        <li><b><a href="/javascript/project/index.html">Project</a></b></li>
+    </ul>
+
+    <ul>
+        <li>(<b class="topic">!intervyu</b>) <b class="topic"><a href="https://www.w3schools.com/js/js_functions.asp"> Function</a></b> - muayyan vazifani bajarish uchun
+            mo'ljallangan kod bloki. Funksiya boshqa joyda chaqirilganda ishka tushadi. Funksiya yordamida kodni bir martta elon qilib, birnecha joyda qayta-qayta ishlatish mumkun.
+            <b>function</b> kalit so'zi orqali elon qilinadi.</li><br>
+        <img src="/javascript/img/function/function.png" alt="function"> <br><br><br>
+
+        <li><b class="topic">Function declaration</b> - function kalit so'zi orqali elon qilinadi, va berilgan <b>scope</b> bo'yicha ko'rinishga ega bo'ladi</li><br>
+        <img src="/javascript/img/function/function-declaration.png" alt="function-declaration"> <br><br><br>
+
+        <li><b class="topic"><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/function"> Function expression</a></b> - function kalit so'zi
+            orqali elon qilinadi, va ma'lum bir o'zgaruvchiga tenglab qo'yish orqali e'lon qilinadi. Function expression elon qilinganidan keyin ishlaydi.</li><br>
+        <img src="/javascript/img/function/function-expression.png" alt="function-expression"> <br><br><br>
+
+        <li><b class="topic"><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions">Arrow function</a></b> - function expression
+            yozilishining qisqaro'q korinishi. Bunday turdagi funksiyalar yozishni turli xil yo'llar mavjud.</li><br>
+        <img src="/javascript/img/function/function-declaration.png" alt="function-declaration"> <br><br><br>
+
+        <h3>Function matter</h3>
+        <img src="/javascript/img/function/fast-or-odd.png" alt="fast-or-odd"> <br><br><br>
+        <img src="/javascript/img/function/the-larger-of-the-two-numbers.png" alt="the-larger-of-the-two-numbers"> <br><br><br>
+        <img src="/javascript/img/function/reverse-thigh-weight.png" alt="reverse-thigh-weight"> <br><br><br>
+
+    </ul>
+</div>
+
+</body>
+</html>
+
