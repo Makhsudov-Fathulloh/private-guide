584c1849ecc5610938f133b6b843f2fa8978abaa
Makhsudov Fathulloh <mr.makhsudov@gmail.com>
2024-04-24 15:23:49 +0500

    add javascript Control-flow

---
+++ javascript/pages/5.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=0);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/css/style.css">
+    <link rel="stylesheet" href="/css/css/main.css">
+
+    <title>Control flow</title>
+</head>
+
+<body>
+
+<div>
+    <ul>
+        <li><b><a href="/index.php">Home</a></b></li>
+        <li><b><a href="/javascript/index.php">Javascript</a></b></li>
+        <li><b><a href="/javascript/project/index.html">Project</a></b></li>
+    </ul>
+
+    <ul>
+        <br>
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/if-else">if/else</a></b></li>
+
+        <h3><span class="red">Conditional statement</span></h3>
+        <img src="/javascript/img/control-flow/1.png" alt="conditional-statement"> <br><br><br>
+
+        <h3><span class="red">If statement</span></h3>
+        <img src="/javascript/img/control-flow/2.png" alt="if-statement"> <br><br><br>
+
+        <h3><span class="red">If/elseif/else statement</span></h3>
+        <img src="/javascript/img/control-flow/3.png" alt="if/elseif/else-statement"> <br><br><br>
+
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/switch-statement">Switch</a></b></li><br>
+        <img src="/javascript/img/control-flow/switch-statement-1.png" alt="switch-statement"> <br><br><br>
+        <img src="/javascript/img/control-flow/switch-statement-2.png" alt="switch-statement" height="750"> <br><br><br>
+        <img src="/javascript/img/control-flow/switch-statement-3.png" alt="switch-statement"> <br><br><br>
+
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/for-loop">For</a></b></li><br>
+        <img src="/javascript/img/control-flow/for-loop-1.png" alt="for-loop"> <br><br><br>
+        <img src="/javascript/img/control-flow/for-loop-2.png" alt="for-loop" height="750"> <br><br><br>
+        <img src="/javascript/img/control-flow/for-loop-3.png" alt="for-loop"> <br><br><br>
+
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/break-statement">break</a></b></li><br>
+        <img src="/javascript/img/control-flow/break-1.png" alt="break-statement"> <br><br><br>
+        <img src="/javascript/img/control-flow/break-2.png" alt="break-statement" height="750"> <br><br><br>
+
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/continue-statement">continue</a></b></li><br>
+        <img src="/javascript/img/control-flow/continue-1.png" alt="continue-statement"> <br><br><br>
+        <img src="/javascript/img/control-flow/continue-2.png" alt="continue-statement" height="750"> <br><br><br>
+
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/while-loop">While</a></b></li><br>
+        <img src="/javascript/img/control-flow/while-loop-1.png" alt="while-loop"> <br><br><br>
+        <img src="/javascript/img/control-flow/while-loop-2.png" alt="while-loop" height="750"> <br><br><br>
+        <img src="/javascript/img/control-flow/while-loop-3.png" alt="while-loop"> <br><br><br>
+
+        <li><b class="topic"><a href="https://www.programiz.com/javascript/while-loop">Do-while</a></b></li><br>
+        <img src="/javascript/img/control-flow/while-loop-1.png" alt="while-loop"> <br><br><br>
+        <img src="/javascript/img/control-flow/while-loop-2.png" alt="while-loop" height="750"> <br><br><br>
+        <img src="/javascript/img/control-flow/while-loop-3.png" alt="while-loop"> <br><br><br>
+
+        <img src="/javascript/img/control-flow/for&while.png" alt="for&while"> <br><br><br>
+
+        <h3>Examples of control flow</h3>
+        <img src="/javascript/img/control-flow/matter-1.png" alt="matter-1"> <br><br><br>
+        <img src="/javascript/img/control-flow/matter-2.png" alt="matter-2"> <br><br><br>
+        <img src="/javascript/img/control-flow/matter-3.png" alt="matter-3"> <br><br><br>
+    </ul>
+</div>
+
+</body>
+</html>
+
